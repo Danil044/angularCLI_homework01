4668afef7d89b39ff7114a605f4c60e36ca6e577
ACER <ACER@DESKTOP-B187N13>
2022-05-05 17:57:24 +0300

    AngularDesign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FormsModule } from "@angular/forms";
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { HeaderCompanentComponent } from './components/header-companent/header-companent.component';
+import { HomeCompanentComponent } from './components/home-companent/home-companent.component';
+import {AppRoutingModele} from "./app-routing.modele";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent,
+    FooterComponentComponent,
+    HeaderCompanentComponent,
+    HomeCompanentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModele
   ],
   providers: [],
   bootstrap: [AppComponent]
